c8326ff01e9845bdb7905c53a5025a226b8dcd18
Xiaoxue Chen <Xiaoxue@Xiaoxues-MacBook-Air.local>
2019-03-10 00:29:12 +0000

    Update

--- src/main/java/com/xiaoxue/domain/Constants.java
+++ src/main/java/com/xiaoxue/domain/Constants.java
@@ -5,10 +5,5 @@ public class Constants {
   private Constants(){}
 
   public static final String EVENTS_KEY = "events";
-  public static final String CODE_KEY = "code";
-  public static final String DESCRIPTION_KEY = "description";
-  public static final String PRIORITY_NAME_KEY = "priority";
-  public static final String SUMMARY_KEY = "summary";
-  public static final String CATEGORY_KEY = "category";
   public static final String DATE_FORMAT = "dd-MM-yyyy hh:mm";
 }
